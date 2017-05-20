544a257212d75ceddc557dba2b21b55b02c4fde5
codyowl <codyowl@gmail.com>
2017-05-21 01:48:38 +0530

    All forms done and integerated in templates :rocket:

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class RecipientForm(forms.Form):
 	name = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Enter Receipt name'}))
 	email = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter Receipt email'}))
+
+class ComposeForm(forms.Form):
+	emailbody = forms.CharField(required=True, widget=forms.Textarea())	
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from dashboard.forms import RecipientForm
+from dashboard.forms import RecipientForm, ComposeForm
 
 def dashboard_home(request, template='dashboard/dashboard_home.html'):
 	return render_to_response(template, context_instance=RequestContext(request))
 
 def compose(request, template='dashboard/compose.html'):
-	return render_to_response(template, context_instance=RequestContext(request))
+	msg = []
+	if request.method == 'POST':
+		form = ComposeForm(request.POST)
+		if form.is_valid():
+			name = form.cleaned_data['name']
+			email = form.cleaned_data['email']
+
+	else:
+		form = ComposeForm()
+
+	context = {
+	'form':form,
+	'msg':msg,
+	}			
+	return render_to_response(template,context,context_instance=RequestContext(request))
 
 def configure(request, template='dashboard/configure.html'):
-	return render_to_response(template, context_instance=RequestContext(request))
+	msg = []
+	if request.method == 'POST':
+		form = RecipientForm(request.POST)
+		if form.is_valid():
+			name = form.cleaned_data['name']
+			email = form.cleaned_data['email']
+
+	else:
+		form = RecipientForm()
+
+	context = {
+	'form':form,
+	'msg':msg,
+	}			
+	return render_to_response(template,context,context_instance=RequestContext(request))
 
 def log(request, template='dashboard/log.html'):
 	return render_to_response(template, context_instance=RequestContext(request))
